5ee0aad3ec2aa7ef0d05fb0796ed4398da3c6c34
Dishi Jain <31739123+dishijn2@users.noreply.github.com>
2019-05-10 23:30:45 +0530

    Add files via upload

---
+++ list,.py
@@ -0,0 +1,19 @@
+n1=int(input())
+n2=int(input())
+list1=[]
+list2=[]
+for i in range(0,n1):
+  a=input()
+  list1.append(a)
+print(list1)
+for j in range(0,n2):
+	b=input()
+	list2.append(b)
+print(list2)
+list3=[]
+if(list[i]==list[j]):
+    x=list1[i]
+    list3.append(x)
+else:
+	print("common words not found")
+
